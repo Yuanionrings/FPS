98628948b252acf4944180b3e1afed738d582c2b
Yuan Gao <yuanionrings@gmail.com>
2020-05-29 17:39:23 -0400

    added player movement

---
+++ Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3f;
+
+    [SerializeField]
+    private float mouseSensitivity = 10f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Calculate Movement
+        float xPosition = Input.GetAxisRaw("Horizontal");
+        float zPosition = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xPosition;
+        Vector3 moveVertical = transform.forward * zPosition;
+
+        // Movement Vector
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (xPosition != 0f || zPosition != 0f))
+        {
+            speed = 1.5f;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl) && (xPosition != 0f || zPosition != 0f)) {
+            speed = 3.5f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            speed = 2.5f;
+        }
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        // Apply Movement
+        motor.SetVelocity(velocity);
+
+        // Calculate X-Rotation
+        float mouseX = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, mouseX, 0f) * mouseSensitivity;
+
+        // Apply Rotation
+        motor.SetRotation(rotation);
+
+        // Calculate Camera Y-Rotation
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(mouseY, 0f, 0f) * mouseSensitivity;
+
+        // Apply Camera Rotation
+        motor.SetCameraRotation(cameraRotation);
+    }
+}
---
+++ Assets/Project/Scripts/PlayerMotor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void SetRotation(Vector3 rotation)
+    {
+        this.rotation = rotation;
+    }
+
+    public void SetCameraRotation(Vector3 cameraRotation)
+    {
+        this.cameraRotation = cameraRotation;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (this.velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + this.velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void Rotate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(this.rotation));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(-this.cameraRotation);
+        }
+    }
+}
